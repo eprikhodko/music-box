d0d970851c618e7971bf5d2a242f158d6f34b9a9
eprikhodko <eprikhodkodev@gmail.com>
2021-09-29 14:44:54 +0300

    Create Header structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { BrowserRouter as Router, Switch } from "react-router-dom"
+
 import Home from "./pages/home"
 
 function App() {
-  return <Home />
+  return (
+    <Router>
+      <Switch>
+        <Home />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,69 @@
+import { useState } from "react"
+
+import { Link } from "react-router-dom"
+import * as ROUTES from "../constants/routes"
+
 function Header() {
-  return <header> I am Header</header>
+  const [searchQuery, setSearchQuery] = useState("")
+
+  const handleChange = (event) => {
+    setSearchQuery(event.target.value)
+  }
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault()
+    console.log("search submitted")
+  }
+
+  console.log(searchQuery)
+
+  return (
+    <header>
+      <div>
+        <Link to={ROUTES.HOME}>
+          <h1>Music Box</h1>
+        </Link>
+
+        <form onSubmit={handleSearchSubmit}>
+          <label htmlFor="search input">
+            Search
+            <input type="text" value={searchQuery} onChange={handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to={ROUTES.HOME} aria-label="Home">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to={ROUTES.CATALOG} aria-label="Catalog">
+                Catalog
+              </Link>
+            </li>
+            <li>
+              <Link to={ROUTES.SEARCH} aria-label="Search">
+                Search
+              </Link>
+            </li>
+            <li>
+              <Link to={ROUTES.LOGIN} aria-label="Login">
+                Log in
+              </Link>
+            </li>
+            <li>
+              <Link to={ROUTES.SIGNUP} aria-label="Signup">
+                Sign up
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
 }
 
 export default Header
