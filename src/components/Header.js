699258aba57eb9db3fa7e89d8af18ce0ed861d53
eprikhodko <eprikhodkodev@gmail.com>
2021-10-04 17:48:00 +0300

    Add border-bottom to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import styled from "styled-components"
+
 import ContainerMain from "./containers/ContainerMain"
 import Container from "./containers/Container"
 import ContainerFlex from "./containers/ContainerFlex"
@@ -5,9 +7,20 @@ import Logo from "./Logo"
 import Search from "./Search"
 import Nav from "./Nav"
 
+const StyledHeader = styled.header`
+  height: 80px;
+  max-width: 1440px;
+  /* width: 90%; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  border-bottom: 3px solid #c2c2c2;
+`
+
 function Header() {
   return (
-    <header>
+    <StyledHeader>
       <ContainerMain>
         <Container>
           <ContainerFlex>
@@ -18,7 +31,7 @@ function Header() {
           <Nav />
         </Container>
       </ContainerMain>
-    </header>
+    </StyledHeader>
   )
 }
 
