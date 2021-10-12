623096d7d153b62e175e988d98dcb6cd15ba5b00
eprikhodko <eprikhodkodev@gmail.com>
2021-10-12 19:37:22 +0300

    Create Album page

--- src/pages/album.js
+++ src/pages/album.js
@@ -2,14 +2,12 @@ import { useParams } from "react-router-dom"
 import Header from "../components/Header"
 
 function Album() {
-  const params = useParams()
-
-  console.log(params)
+  const { albumId } = useParams()
 
   return (
     <>
       <Header />
-      <div>Hello, this is page of </div>
+      <div>Hello, this is page of an album with ID number of {albumId}</div>
     </>
   )
 }
