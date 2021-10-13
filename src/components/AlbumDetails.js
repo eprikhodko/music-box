cf96ba2066c456b78f6c67d215b2eec9e6754c79
eprikhodko <eprikhodkodev@gmail.com>
2021-10-13 21:08:59 +0300

    Create initial AlbumDetails component structure

--- src/components/AlbumDetails.js
+++ src/components/AlbumDetails.js
@@ -1,9 +1,28 @@
+import { useContext } from "react"
 import { useParams } from "react-router-dom"
+import AlbumsDataContext from "../context/albumsData"
 
 function AlbumDetails() {
   const { albumId } = useParams()
+  const { albumsData } = useContext(AlbumsDataContext)
 
-  return <div>Hello, this is page of an album with ID number of {albumId}</div>
+  console.log(albumId)
+  console.log(albumsData)
+
+  const album = albumsData.find((element) => element.albumId === albumId)
+
+  console.log(album)
+
+  return (
+    <div>
+      Hello, this is page of an album with ID number of {albumId}.
+      <img src={album.albumCover} alt={album.albumCover} />
+      <h2>{album.albumTitle}</h2>
+      <p>{album.artist}</p>
+      <p>{album.year}</p>
+      <p>{album.genre}</p>
+    </div>
+  )
 }
 
 export default AlbumDetails
