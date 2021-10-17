999d2cf808e3099d44ae2ff64c29afe17b85dc78
eprikhodko <eprikhodkodev@gmail.com>
2021-10-17 22:49:20 +0300

    Rearrange sign up form inputs

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -15,42 +15,6 @@ const StyledForm = styled.form`
   /* border: 2px solid goldenrod; */
 `
 
-// const ContainerInput = styled.div`
-//   display: flex;
-//   /* flex-direction: column; */
-//   /* border: 1px solid; */
-// `
-
-// const FormInput = styled.input`
-//   min-width: 564px;
-//   font-family: inherit;
-//   color: rgba(0, 0, 0, 0.7);
-//   font-size: 2.5rem;
-//   font-weight: 500;
-//   /* line-height: 4; */
-//   background-color: transparent;
-//   border: 0;
-//   border-bottom: 3px solid #c2c2c2;
-//   outline: 3px solid transparent;
-//   /* margin: 4em 0; */
-//   /* border: 1px solid green; */
-//   &::placeholder {
-//     /* add "opacity: 1;" rule because firefox will render input placeholder semi-transparent */
-//     opacity: 1;
-//     color: rgba(0, 0, 0, 0.7);
-//     font-size: 2.5rem;
-//     font-weight: 500;
-//   }
-// `
-
-// const FormLabel = styled.label`
-//   color: rgba(0, 0, 0, 0.7);
-//   font-size: 2.5rem;
-//   font-weight: 500;
-//   margin: 0;
-//   /* border: 1px solid red; */
-// `
-
 const FloatLabel = styled.label`
   font-size: 2.5rem;
   color: rgba(0, 0, 0, 0.7);
@@ -59,7 +23,7 @@ const FloatLabel = styled.label`
   /* padding: 0 12px; */
   pointer-events: none;
   position: absolute;
-  /* translate() method moves label 0 pixels to the right, and 0.1 em up from the border bottom */
+  /* translate() method moves label 0 pixels to the right, and 0.1 em up from the border bottom of FloatInput */
   /* https://www.w3schools.com/css/css3_2dtransforms.asp */
   /* transform: translate(0, -0.1em) scale(1); */
 
@@ -72,7 +36,7 @@ const FloatLabel = styled.label`
   background-color: ${({ email }) => email && "red"}; */
 `
 
-const ContainerFloatLabel = styled.div`
+const ContainerFloatInput = styled.div`
   display: flex;
   flex-direction: column;
   min-width: 564px;
@@ -91,17 +55,17 @@ const FloatInput = styled.input`
   font-weight: 500;
   font-family: "Inter", sans-serif;
   /* line-height: 1.5; */
-  padding-top: 0.2em;
+  /* padding-top: 0.2em; */
+  padding: 0;
+  padding-bottom: 0.15em;
   /* width: 100%; */
   /* height: 56px; */
-  /* padding: 14px 16px 0 10px; */
-  /* outline: 0; */
   outline: 3px solid transparent;
   border: 0;
   border-bottom: 3px solid #c2c2c2;
   background: transparent;
-  // border: 3px solid red;
   /* background-color: #fff; */
+  /* border: 3px solid red; */
   /* below is a solution for a browser colored autofill input background and text color
   https://selleo.com/til/posts/xhgzlfgcql-a-way-to-make-autofilled-inputs-background-transparent */
   &:-webkit-autofill,
@@ -124,18 +88,18 @@ function SignUpForm() {
     console.log("clicked")
   }
 
-  console.log(username)
-
   return (
     <ContainerMain>
       <Content>
         <CenterContent>
           <StyledForm onSubmit={handleSubmit}>
-            <ContainerFloatLabel>
+            <ContainerFloatInput>
+              <FloatLabel htmlFor="username" isNotEmpty={username}>
+                Username
+              </FloatLabel>
               <FloatInput
                 id="username"
                 type="text"
-                // placeholder="Username"
                 name="username"
                 aria-label="Username"
                 value={username}
@@ -143,12 +107,12 @@ function SignUpForm() {
                   setUsername(event.target.value.toLowerCase())
                 }}
               />
-              <FloatLabel htmlFor="username" isNotEmpty={username}>
-                Username
-              </FloatLabel>
-            </ContainerFloatLabel>
+            </ContainerFloatInput>
 
-            <ContainerFloatLabel>
+            <ContainerFloatInput>
+              <FloatLabel htmlFor="email" isNotEmpty={email}>
+                Email
+              </FloatLabel>
               <FloatInput
                 id="email"
                 type="email"
@@ -159,22 +123,43 @@ function SignUpForm() {
                   setEmail(event.target.value.toLowerCase())
                 }}
               />
-              <FloatLabel htmlFor="email" isNotEmpty={email}>
-                Email
+            </ContainerFloatInput>
+
+            <ContainerFloatInput>
+              <FloatLabel htmlFor="password" isNotEmpty={password}>
+                Password
               </FloatLabel>
-            </ContainerFloatLabel>
-            <label>
-              <input
-                type="email"
-                placeholder="Email"
-                name="emailAddress"
-                aria-label="Email address"
-                value={email}
+              <FloatInput
+                id="password"
+                type="password"
+                name="password"
+                aria-label="Password"
+                value={password}
                 onChange={(event) => {
-                  setEmail(event.target.value.toLowerCase())
+                  setPassword(event.target.value)
                 }}
               />
-            </label>
+            </ContainerFloatInput>
+
+            <ContainerFloatInput>
+              <FloatLabel
+                htmlFor="confirmPassword"
+                isNotEmpty={confirmPassword}
+              >
+                Confirm password
+              </FloatLabel>
+              <FloatInput
+                id="confirmPassword"
+                type="password"
+                name="confirmPassword"
+                aria-label="Confirm password"
+                value={confirmPassword}
+                onChange={(event) => {
+                  setConfirmPassword(event.target.value)
+                }}
+              />
+            </ContainerFloatInput>
+
             <input
               type="password"
               placeholder="Password (6 characters minimum)"
@@ -185,16 +170,7 @@ function SignUpForm() {
                 setPassword(event.target.value.toLowerCase())
               }}
             />
-            <input
-              type="password"
-              placeholder="Confirm password"
-              name="confirmPassword"
-              aria-label="Confirm password"
-              value={confirmPassword}
-              onChange={(event) => {
-                setConfirmPassword(event.target.value.toLowerCase())
-              }}
-            />
+
             <button
               type="submit"
               onSubmit={handleSubmit}
@@ -208,18 +184,6 @@ function SignUpForm() {
           </StyledForm>
           <p>Already have an account?</p>
           <Link to={ROUTES.LOGIN}>Log in</Link>
-          <ContainerFloatLabel>
-            <FloatInput
-              type="email"
-              onChange={(event) => {
-                setEmail(event.target.value.toLowerCase())
-              }}
-            />
-            {/* In order to make styled component 'FloatLabel' change its style depending on 'email' state, we need to pass to it 'email' state as props. We can then use this 'email' prop to control FloatLabel component styles. If we type something into FloatInput input, email state will change, and FloatLabel will change its style. Great! */}
-            <FloatLabel htmlFor="email" email={email}>
-              Email
-            </FloatLabel>
-          </ContainerFloatLabel>
         </CenterContent>
       </Content>
     </ContainerMain>
