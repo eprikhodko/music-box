ae47ea8842801fb7208eed09771816e6823f5433
eprikhodko <eprikhodkodev@gmail.com>
2021-10-25 14:00:11 +0300

    Create more simple Button component

---
+++ src/components/shared/Button.js
@@ -0,0 +1,34 @@
+import styled from "styled-components"
+
+const Button = styled.button`
+  font-family: "Inter", sans-serif;
+  font-weight: 500;
+  font-size: 1.6rem;
+  line-height: 1.25;
+  color: #333;
+
+  padding: 1em 3.5em;
+  /* disable padding left and right if button recieve a 'hero' prop */
+  padding: ${({ hero }) => hero && "1em 0"};
+  /* set min-width if button receive a 'hero' prop */
+  min-width: ${({ hero }) => hero && "10.5em"};
+  background-color: transparent;
+  cursor: pointer;
+  border: 2px solid #333;
+  border-radius: 50px;
+  margin-right: ${({ marginRight }) => marginRight};
+  margin-top: ${({ marginTop }) => marginTop || "10em"};
+  &:hover {
+    background-color: #333;
+    color: #fff;
+  }
+  &:focus {
+    background-color: #333;
+    color: #fff;
+    outline: 3px solid transparent;
+    /* outline: 2px solid #333; */
+    /* outline-offset: 4px; */
+  }
+`
+
+export { Button as default }
--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,11 +1,35 @@
+import styled from "styled-components"
+// import * as ROUTES from "../constants/routes"
 import Footer from "../components/Footer"
 import Header from "../components/Header/index"
+import { ContainerMain, Content } from "../components/shared/Containers"
+// import Button from "../components/shared/buttons/Button"
+import Button from "../components/shared/Button"
+
+const NotFoundTitle = styled.h1`
+  font-size: 28rem;
+  line-height: 1.4;
+`
+
+const NotFoundSubTitle = styled.h2`
+  font-size: 4.5rem;
+  font-weight: 500;
+`
 
 function NotFound() {
   return (
     <>
       <Header />
-      <div>Sorry, page not found</div>
+      <main>
+        <ContainerMain>
+          <Content flexDirection="column" alignItems="center" marginTop="2em">
+            <NotFoundTitle>404</NotFoundTitle>
+            <NotFoundSubTitle>Sorry, this page not found</NotFoundSubTitle>
+            {/* <Button as="a" href="/search" text="text" /> */}
+            <Button marginTop="2em">hello</Button>
+          </Content>
+        </ContainerMain>
+      </main>
       <Footer />
     </>
   )
