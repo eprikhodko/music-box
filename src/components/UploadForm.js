a5f79f13ed75e43566e2e280ada95193648583b7
eprikhodko <eprikhodkodev@gmail.com>
2021-10-29 21:44:27 +0300

    Add image upload file input

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -10,9 +10,23 @@ import { Button } from "./shared/Button"
 import { ReactComponent as IconImagePlaceholder } from "../icons/image-placeholder.svg"
 import { ReactComponent as CheckboxCircleIcon } from "../icons/check_circle_24px.svg"
 
-const ImageUpload = styled.div`
-  width: 35em;
-  height: 35em;
+const ContainerUploadForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* set default margin-top for StyledForm component */
+  margin-top: ${({ marginTop }) => marginTop || "10em"};
+  /* border: 2px solid goldenrod; */
+`
+
+const UploadImageStyledLabel = styled.label`
+  font-size: 1.8rem;
+  color: rgba(0, 0, 0, 0.5);
+  font-weight: 500;
+  margin-top: 0.6em;
+
+  width: 31em;
+  height: 31em;
   background-color: #c2c2c2;
 
   display: flex;
@@ -22,7 +36,6 @@ const ImageUpload = styled.div`
 
   will-change: transform;
   transition: all 450ms;
-
   &:hover {
     cursor: pointer;
     background-color: rgba(0, 0, 0, 0.2);
@@ -33,11 +46,16 @@ const ImageUpload = styled.div`
   }
 `
 
-const UploadIconText = styled.p`
-  font-size: 1.8rem;
-  color: rgba(0, 0, 0, 0.5);
-  font-weight: 500;
-  margin-top: 0.6em;
+const ImagePlaceholderIcon = styled(IconImagePlaceholder)`
+  width: 3em;
+  height: 3em;
+  margin-bottom: 1em;
+`
+
+const HiddenFileInput = styled.input`
+  height: 0;
+  padding: 0;
+  opacity: 0;
 `
 
 const CheckboxLabel = styled.label`
@@ -88,20 +106,6 @@ const ContainerCheckboxes = styled.div`
   /* border: 1px solid green; */
 `
 
-const ContainerUploadForm = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  /* set default margin-top for StyledForm component */
-  margin-top: ${({ marginTop }) => marginTop || "10em"};
-  /* border: 2px solid goldenrod; */
-`
-
-const ImagePlaceholderIcon = styled(IconImagePlaceholder)`
-  width: 3.4em;
-  height: 3.4em;
-`
-
 function UploadForm() {
   const [albumName, setAlbumName] = useState("")
   const [artistName, setArtistName] = useState("")
@@ -117,10 +121,11 @@ function UploadForm() {
     <Content justifyContent="center">
       <form onSubmit={handleSubmit}>
         <ContainerUploadForm marginTop="5em">
-          <ImageUpload>
+          <UploadImageStyledLabel htmlFor="imageUpload">
             <ImagePlaceholderIcon />
-            <UploadIconText>Click to upload album picture</UploadIconText>
-          </ImageUpload>
+            Click to upload album picture
+            <HiddenFileInput id="imageUpload" name="imageUpload" type="file" />
+          </UploadImageStyledLabel>
 
           <ContainerFloatInput marginTop="3em">
             <FloatLabel htmlFor="albumName" isNotEmpty={albumName}>
@@ -210,7 +215,7 @@ function UploadForm() {
               name="addToWishlist"
             />
             <CustomCheckbox />
-            add to my wishlist
+            add to wishlist
           </CheckboxLabel>
         </ContainerCheckboxes>
 
