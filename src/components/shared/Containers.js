3663e269ed1d85b89653ee2f74c0f87d72c9566b
eprikhodko <eprikhodkodev@gmail.com>
2021-10-31 16:04:36 +0300

    Make footer stick to the bottom of the page

--- src/components/shared/Containers.js
+++ src/components/shared/Containers.js
@@ -4,7 +4,13 @@ export const ContainerMain = styled.div`
   width: 90%;
   max-width: 90em;
   margin: 0 auto;
-  /* border: 5px solid goldenrod; */
+  border: 5px solid goldenrod;
+
+  /* ########## make Footer stick to the bottom of the page ########## */
+  /* set ContainerMain height to 100vh. This means that ContainerMain will take 100% of viewport height*/
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
 `
 
 export const Content = styled.div`
--- src/pages/collection.js
+++ src/pages/collection.js
@@ -1,13 +1,25 @@
+import styled from "styled-components"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
+import { ContainerMain } from "../components/shared/Containers"
+
+const PageBody = styled.main`
+  /* flex-grow defines how much a flexbox item should grow if there's space available */
+  /* make PageBody fill up all remaining space if no other flexbox item has a flex-grow value*/
+  flex-grow: 1;
+`
 
 function Collection() {
   return (
-    <>
+    <ContainerMain>
       <Header />
-      collection content
+      <PageBody>
+        <div>Collection title</div>
+        <div>collection grid</div>
+        <button type="button">show more</button>
+      </PageBody>
       <Footer />
-    </>
+    </ContainerMain>
   )
 }
 
