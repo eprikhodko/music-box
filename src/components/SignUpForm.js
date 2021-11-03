7e076d4c28de7e25a86df0dcf91b32d50282c45a
eprikhodko <eprikhodkodev@gmail.com>
2021-11-03 16:10:58 +0300

    Install firebase app according to official docs

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -2,8 +2,12 @@ import { Link } from "react-router-dom"
 import { useState } from "react"
 import styled from "styled-components"
 
+// install firebase according to official docs: https://firebase.google.com/docs/web/setup, don't use "/lib/firebase.js"
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
 import { initializeApp } from "firebase/app"
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
+// import { getFirestore, collection } from "firebase/firestore"
 
 import * as ROUTES from "../constants/routes"
 import { Button } from "./shared/Button"
@@ -14,14 +18,6 @@ import {
   Form,
 } from "./shared/FormElements"
 
-// Follow this pattern to import other Firebase services
-// import { } from 'firebase/<service>';
-
-// TODO: Replace the following with your app's Firebase project configuration
-// import "firebase/auth"
-import "firebase/firestore"
-import "firebase/storage"
-
 const firebaseConfig = {
   apiKey: "AIzaSyCu_RkOy-eiFqNWBPr_tEEW4zY8JVYg-tg",
   authDomain: "music-box-e8f66.firebaseapp.com",
@@ -30,12 +26,6 @@ const firebaseConfig = {
   messagingSenderId: "1039275884942",
   appId: "1:1039275884942:web:f643baf07a740cf5c5a4df",
 }
-
-const firebaseApp = initializeApp(firebaseConfig)
-const auth = getAuth(firebaseApp)
-// const db = firebaseApp.firestore()
-// console.log(firebaseApp)
-// console.log(auth)
 
 // import FirebaseContext from "../context/firebase"
 
@@ -52,54 +42,39 @@ const StyledLink = styled(Link)`
   }
 `
 
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+
 function SignUpForm() {
-  // console.log(db)
   const [username, setUsername] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
   // const { firebase } = useContext(FirebaseContext)
-  // console.log(firebase.auth())
 
   const handleSignup = async (event) => {
     event.preventDefault()
+    // const auth = getAuth()
+    try {
+      // create new user account in the firebase authentication
+      const createdUser = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+      console.log(createdUser.user)
 
-    const createdUser = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    )
-    console.log(createdUser)
-
-    // const createUser = async () => {
-    //   await createUserWithEmailAndPassword(auth, email, password)
-    //     .then((userCredential) => {
-    //       // Signed in
-    //       const { user } = userCredential
-    //       console.log(user)
-    //     })
-    //     .catch((error) => {
-    //       const errorCode = error.code
-    //       const errorMessage = error.message
-    //       console.log(errorCode)
-    //       console.log(errorMessage)
-    //     })
-    // }
-
-    // createUser()
-
-    // createUserWithEmailAndPassword(auth, email, password)
-    //   .then((userCredential) => {
-    //     // Signed in
-    //     const { user } = userCredential
-    //     console.log(user)
-    //   })
-    //   .catch((error) => {
-    //     const errorCode = error.code
-    //     const errorMessage = error.message
-    //     console.log(errorCode)
-    //     console.log(errorMessage)
-    //   })
+      // const db = getFirestore(firebaseApp)
+      // const createUserInFirestoreDatabase = await
+      // collection("users").doc(createdUser.user.uid).set({
+      //   userId: createdUser.user.uid,
+      //   username: username.toLowerCase(),
+      //   email: email.toLowerCase(),
+      //   dateCreated: Date.now(),
+      // })
+    } catch (error) {
+      console.log(error)
+    }
 
     console.log("Form submitted")
   }
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,24 +1,14 @@
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-import { initializeApp } from "firebase/app"
-// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
-// import * as firebase from "firebase/app"
+// import { initializeApp } from "firebase/app"
 
-// Add the Firebase products that you want to use
-// if we don't import "firebase/auth, we will get an error when we try to call firebase.auth(), -> firebase.auth is not a function
-import "firebase/auth"
-import "firebase/firestore"
-import "firebase/storage"
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCu_RkOy-eiFqNWBPr_tEEW4zY8JVYg-tg",
+//   authDomain: "music-box-e8f66.firebaseapp.com",
+//   projectId: "music-box-e8f66",
+//   storageBucket: "music-box-e8f66.appspot.com",
+//   messagingSenderId: "1039275884942",
+//   appId: "1:1039275884942:web:f643baf07a740cf5c5a4df",
+// }
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCu_RkOy-eiFqNWBPr_tEEW4zY8JVYg-tg",
-  authDomain: "music-box-e8f66.firebaseapp.com",
-  projectId: "music-box-e8f66",
-  storageBucket: "music-box-e8f66.appspot.com",
-  messagingSenderId: "1039275884942",
-  appId: "1:1039275884942:web:f643baf07a740cf5c5a4df",
-}
+// const firebaseApp = initializeApp(firebaseConfig)
 
-// Initialize Firebase
-const firebase = initializeApp(firebaseConfig)
-
-export default firebase
+// export default firebaseApp
