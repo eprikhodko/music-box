58e2ecb55c5d79d494aaa81556d163d6ec9275c6
eprikhodko <eprikhodkodev@gmail.com>
2021-11-16 12:50:35 +0300

    Show albums in user collection when user visit link to his albums collection

--- src/pages/collection.js
+++ src/pages/collection.js
@@ -1,3 +1,6 @@
+import styled from "styled-components"
+import { Link } from "react-router-dom"
+
 import { useState, useEffect, useContext } from "react"
 import {
   getFirestore,
@@ -10,6 +13,7 @@ import {
 import UserContext from "../context/user"
 import AlbumsDataContext from "../context/albumsData"
 
+import * as ROUTES from "../constants/routes"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import CollectionGrid from "../components/shared/grids/CollectionGrid"
@@ -20,11 +24,21 @@ import {
   PageBody,
 } from "../components/shared/Containers"
 
+const StyledParagraph = styled.p`
+  font-size: 2.5rem;
+  text-align: center;
+  width: 40em;
+`
+
+const StyledLink = styled(Link)`
+  color: #000;
+`
+
 function Collection() {
   const currentUser = useContext(UserContext)
   const { albumsData } = useContext(AlbumsDataContext)
 
-  const [albums, setAlbums] = useState([])
+  const [albumsInUserCollection, setAlbumsInUserCollection] = useState([])
   const [albumsSlice, setAlbumsSlice] = useState({
     start: 0,
     end: 11,
@@ -32,10 +46,10 @@ function Collection() {
 
   const fetchAlbumsInUserCollection = async () => {
     const db = getFirestore()
-    // const querySnapshot = await getDocs(
+    // const querySnap = await getDocs(
     //   collection(db, "users", currentUser.uid, "albumsInUserCollection")
     // )
-    // const docsData = querySnapshot.docs.map((doc) => doc.data())
+    // const docsData = querySnap.docs.map((doc) => doc.data())
 
     const albumsRef = collection(
       db,
@@ -45,12 +59,18 @@ function Collection() {
     )
     const q = query(albumsRef, orderBy("dateAdded", "desc"), limit(100))
     const querySnapshot = await getDocs(q)
+
     // map through albums and return new array with albums ids
     const albumsIDsList = querySnapshot.docs.map((doc) => doc.data().albumId)
 
     // create new filteredAlbums array which consists of albums which ids match ids in albumsIDsList array
     const filteredAlbums = albumsData.filter((album) =>
       // run code below for each album in albumsData array and return only those albums which ids match with ids in albumsIDsList
+      // for each album in albumsData array test if this album.albumId is in provided albumsIDsList array
+      // if true, then .includes() method will return true, and therefore .filter() will return this album to the filteredAlbums array
+      // .includes() method takes albumsIDsList and compare each id inside albumsIDsList with provided album.albumId
+      // if album.albumId matches with id inside albumsIDsList, then .includes() will return 'true'
+      // then if .includes() method returned 'true', the album inside albumsData which albumId matched with id in albumsIDsList will appear in filteredAlbums array
       albumsIDsList.includes(album.albumId)
     )
 
@@ -63,18 +83,19 @@ function Collection() {
     }
     // sort albums
     const sortedAlbums = filteredAlbums.sort(sortFunc)
-    setAlbums(sortedAlbums)
+
+    // write sorted albums to the state
+    setAlbumsInUserCollection(sortedAlbums)
   }
 
   useEffect(() => {
     // fetch albums which are in user collection
     if (currentUser && albumsData) fetchAlbumsInUserCollection()
-    // pass this albums that are saved in the state to the CollectionGrid child component
     // acceptance criteria:
     // if albums in the collection <1 , show text block "add some albums to your collection and they will show up here"
     // if albums in the collection <12, don't show "show more" button
 
-    // fetch albums data after useAuth hook change currentUser from 'null' to authorized user
+    // fetch albums in user collection after albumsData loaded to the state
   }, [albumsData])
 
   const handleShowMore = () => {
@@ -86,10 +107,27 @@ function Collection() {
       <PageBody>
         <Content flexDirection="column" alignItems="center" $marginTop="5em">
           <h2>Collection</h2>
-          <CollectionGrid albumsSlice={albumsSlice} albumsData={albums} />
-          <Button marginTop="2em" onClick={handleShowMore}>
-            Show more
-          </Button>
+          {albumsInUserCollection.length > 0 ? (
+            <>
+              <CollectionGrid
+                albumsSlice={albumsSlice}
+                albumsData={albumsInUserCollection}
+              />
+              {/* Show 'Show more' button only if there is more then 12 albums in user collection */}
+              {albumsInUserCollection.length > 11 && (
+                <Button marginTop="2em" onClick={handleShowMore}>
+                  Show more
+                </Button>
+              )}
+            </>
+          ) : (
+            <StyledParagraph>
+              There is no albums in your collection yet. Would you like to add
+              some from <StyledLink to={ROUTES.CATALOG}>catalog</StyledLink> or
+              maybe <StyledLink to={ROUTES.UPLOAD}>upload</StyledLink> a new
+              album yourself?
+            </StyledParagraph>
+          )}
         </Content>
       </PageBody>
       <Footer />
