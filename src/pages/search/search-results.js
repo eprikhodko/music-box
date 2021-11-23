3c2026c3e690fff71291eea5a5acaad9d9bdc012
eprikhodko <eprikhodkodev@gmail.com>
2021-11-23 11:15:10 +0300

    Filter albums which genre includes search query from search page

--- src/pages/search/search-results.js
+++ src/pages/search/search-results.js
@@ -1,12 +1,30 @@
+import { useContext } from "react"
 import { useParams } from "react-router-dom"
+
+import AlbumsDataContext from "../../context/albumsData"
 
 function SearchResults() {
   const { searchQuery } = useParams()
 
+  const { albumsData } = useContext(AlbumsDataContext)
+
+  console.log(albumsData)
+  // get albums data from context
+  // find 'searchQuery' string in albumsData array of objects in album.genre string
+  // return only that albums that match searchQuery
+  // map this albums objects in an albums grid
+  // render this albums components
+
+  const filteredAlbums = albumsData.filter((album) =>
+    album.genre.toLowerCase().includes(searchQuery.toLowerCase())
+  )
+
+  console.log(filteredAlbums)
+
   return (
     <h2>
       search results for &quot;{searchQuery}&quot; search query should appear
-      here
+      here <p>if nothing found, show nothing found</p>
     </h2>
   )
 }
