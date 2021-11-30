2c0c11517ac792584072fe799f2ade7970ff7c09
eprikhodko <eprikhodkodev@gmail.com>
2021-11-30 18:40:44 +0300

    Add useMatchMedia hook

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,14 +3,17 @@ import Hero from "../components/Hero"
 import RecentlyAddedAlbums from "../components/RecentlyAddedAlbums"
 import Footer from "../components/Footer"
 import { ContainerMain, PageBody } from "../components/shared/Containers"
+import useMatchMedia from "../hooks/useMatchMedia"
 
 function Home() {
+  const isMobileResolution = useMatchMedia("(min-width: 400px)", true)
+  console.log(isMobileResolution)
   return (
     <ContainerMain>
       <Header />
       <PageBody>
         <Hero />
-        <RecentlyAddedAlbums />
+        {isMobileResolution && <RecentlyAddedAlbums />}
       </PageBody>
       <Footer />
     </ContainerMain>
