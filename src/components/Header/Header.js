e40cbd67182e5ab36fca85b3e62fc90bf984b830
eprikhodko <eprikhodkodev@gmail.com>
2021-12-02 17:39:07 +0300

    Create initial hamburger menu prototype

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,9 @@
 import PropTypes from "prop-types"
 import styled from "styled-components"
 
+import { Link } from "react-router-dom"
+import * as ROUTES from "../../constants/routes"
+
 import { Content } from "../shared/Containers"
 import Logo from "./Logo"
 import { ReactComponent as Hamburger } from "../../icons/burger.svg"
@@ -18,11 +21,52 @@ const ContainerFlex = styled.div`
   align-items: center;
 `
 
+const HamburgerMenu = styled.nav`
+  background-color: #333;
+  width: 200px;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  right: 0;
+  /* right: -25%; */
+  /* right: -100%; */
+  z-index: 1;
+`
+
+const Ul = styled.ul`
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+  padding: 0 1em;
+`
+
+const NavLink = styled(Link)`
+  color: #fff;
+  font-size: 3rem;
+  font-weight: 600;
+  text-decoration: none;
+  /* padding-bottom: 0.2em; */
+  /* margin-left: 2.5em; */
+
+  &:hover {
+    border-bottom: 2px solid #000;
+  }
+  &:focus {
+    border-bottom: 2px solid #000;
+    outline: 3px solid transparent;
+  }
+`
+
 function Header({ noSearchBox }) {
   const isDesktopResolution = useMatchMedia("(min-width: 400px)", false)
   console.log(isDesktopResolution)
 
   const showSearchBox = !noSearchBox
+
+  const handleClick = () => {
+    console.log("clicked")
+  }
 
   return (
     <StyledHeader>
@@ -36,9 +80,32 @@ function Header({ noSearchBox }) {
           <Nav />
         ) : (
           <div>
-            <Hamburger />
+            <Hamburger onClick={handleClick} />
           </div>
         )}
+        <HamburgerMenu>
+          <Ul aria-label="Header navigation" role="navigation">
+            <li>
+              <NavLink to={ROUTES.HOME}>Home</NavLink>
+            </li>
+
+            <li>
+              <NavLink to={ROUTES.CATALOG}>Catalog</NavLink>
+            </li>
+
+            <li>
+              <NavLink to={ROUTES.SEARCH}>Search</NavLink>
+            </li>
+
+            <li>
+              <NavLink to={ROUTES.LOGIN}>Log in</NavLink>
+            </li>
+
+            <li>
+              <NavLink to={ROUTES.SIGNUP}>Sign up</NavLink>
+            </li>
+          </Ul>
+        </HamburgerMenu>
       </Content>
     </StyledHeader>
   )
